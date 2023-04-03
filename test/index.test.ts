9501bb10a1bfbd55a4b2f2371374280561c59396
fratzinger <22286818+fratzinger@users.noreply.github.com>
2023-04-03 08:24:47 +0200

    test: fix test

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,7 @@ import { GoogleMapsService } from '../src';
 describe('index.test.ts', function () {
   it('get started', function () {
     // @ts-expect-error test
-    const googleMapsService = new GoogleMapsService();
+    const googleMapsService = new GoogleMapsService({});
     expect(googleMapsService).toBeInstanceOf(GoogleMapsService);
   });
 });
